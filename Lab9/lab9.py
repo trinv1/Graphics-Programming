6cdf16adb4464490cbef45281d35b990288241e1
trinv1 <trinityvillaruel@gmail.com>
2024-11-22 18:18:16 +0000

    displaying sobel images

--- Lab9/lab9.py
+++ Lab9/lab9.py
@@ -7,28 +7,35 @@ img = cv2.imread('ATU.jpg',)
 #Convert the image to grayscale
 gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+#Converting images to sobel
+sobelHorizontal = cv2.Sobel(img, cv2.CV_64F,1,0,ksize=5)  # x dir
+sobelVertical = cv2.Sobel(img,cv2.CV_64F,0,1,ksize=5)  # y dir 
+
 #Blurring images
 grayBlurredImage = cv2.GaussianBlur(gray_image,(9, 9),0) #Blurring gray image
 originalBlurredImage = cv2.GaussianBlur(img, (5, 5), 0) #Blurring original image
 
 #Plotting 4 images in window
-plt.subplot(2, 2,1),plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+plt.subplot(3, 2,1),plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
 plt.title('Original'), plt.xticks([]), plt.yticks([])
 
-plt.subplot(2, 2, 3), plt.imshow(gray_image, cmap='gray')
+plt.subplot(3, 2, 3), plt.imshow(gray_image, cmap='gray')
 plt.title('GrayScale'), plt.xticks([]), plt.yticks([])
 
-plt.subplot(2, 2, 2), plt.imshow(cv2.cvtColor(originalBlurredImage, cv2.COLOR_BGR2RGB))
+#Plotting blurred images
+plt.subplot(3, 2, 2), plt.imshow(cv2.cvtColor(originalBlurredImage, cv2.COLOR_BGR2RGB))
 plt.title('5x5 Blur'), plt.xticks([]), plt.yticks([])
 
-plt.subplot(2, 2, 4), plt.imshow(grayBlurredImage, cmap='gray')
+plt.subplot(3, 2, 4), plt.imshow(grayBlurredImage, cmap='gray')
 plt.title('9x9 Blur'), plt.xticks([]), plt.yticks([])
+
+#Plotting sobel images
+plt.subplot(3, 2,5),plt.imshow(sobelHorizontal, cmap='gray')
+plt.title('Sobel X'), plt.xticks([]), plt.yticks([])
+
+plt.subplot(3, 2, 6), plt.imshow(sobelVertical, cmap='gray')
+plt.title('Sobel Y'), plt.xticks([]), plt.yticks([])
 
 plt.show() 
 
-#Displaying blurred imaged in opencv
-cv2.imshow('9x9 Blur', grayBlurredImage)
-cv2.imshow('3x3 Blur', originalBlurredImage)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
 
