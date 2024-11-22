8b85969d89d401af807aef84d4323cb332e814b6
trinv1 <trinityvillaruel@gmail.com>
2024-11-22 17:30:41 +0000

    plotted 2 images in window, 1 in colour, 1 in gray scale

--- Lab9/lab9.py
+++ Lab9/lab9.py
@@ -10,5 +10,11 @@ gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 #Display the grayscale image
 cv2.imshow('Grayscale Image', gray_image)
 cv2.waitKey(0)
-cv2.destoryAllWindows()
+cv2.destroyAllWindows()
 
+#Plotting 2 images in window
+plt.subplot(1, 2,1),plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB)) 
+plt.title('Original'), plt.xticks([]), plt.yticks([]) 
+plt.subplot(1, 2,2),plt.imshow(gray_image, cmap = 'gray') 
+plt.title('GrayScale'), plt.xticks([]), plt.yticks([]) 
+plt.show() 
