8262e413e70c60918ff493d80b7119a4f3d21d3b
trinv1 <trinityvillaruel@gmail.com>
2024-11-26 10:43:30 +0000

    plotted orb image

--- Lab10/lab10.py
+++ Lab10/lab10.py
@@ -3,16 +3,24 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 img = cv2.imread('ATU1.jpg',) 
+imgBuilding = cv2.imread('Building.jpg',) 
 
 #Converting image to grayscale
 gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+gray_image2 = cv2.cvtColor(imgBuilding, cv2.COLOR_BGR2GRAY)
 
 #Copying original image
 imgHarris = img.copy()
 imgShiTomasi = img.copy()
+imgORB = img.copy()
+
+imgHarris2 = imgBuilding.copy()
+imgShiTomasi2 = imgBuilding.copy()
+imgORB2 = imgBuilding.copy()
 
 #Performing corner detection 
 dst = cv2.cornerHarris(gray_image, 2, 3, k = 0.04) 
+dst = cv2.cornerHarris(gray_image2, 2, 3, k = 0.04) 
 
 #Setting threashold for marking corners
 threshold = 0.3; #number between 0 and 1 
@@ -20,16 +28,29 @@ for i in range(len(dst)):
     for j in range(len(dst[i])): 
         if dst[i][j] > (threshold*dst.max()): 
             cv2.circle(imgHarris,(j,i),3,(0, 255, 0),-1) 
+            cv2.circle(imgHarris2,(j,i),3,(0, 255, 0),-1) 
 
 #Doing Shi Tomasi corner detection
 corners = cv2.goodFeaturesToTrack(gray_image, 40, 0.01, 10) 
-corners = np.int0(corners) 
+corners= cv2.goodFeaturesToTrack(gray_image2, 40, 0.01, 10) 
 
+#Looping through corners
 for i in corners: 
     x,y = i.ravel() 
     cv2.circle(imgShiTomasi,(x,y),3,(0, 255, 0),-1) 
 
 plt.figure(figsize=(10, 15))  #Width=10, Height=15 
+
+#Initiating ORB detector
+orb = cv2.ORB_create()
+ 
+#Finding the keypoints with ORB
+kp = orb.detect(imgORB,None)
+kp = orb.detect(imgORB2,None)
+ 
+#Computing the descriptors with ORB
+kp, des = orb.compute(imgORB, kp)
+kp, des = orb.compute(imgORB2, kp)
 
 #Plotting grayscale image
 plt.subplot(2, 2, 1), plt.imshow(gray_image, cmap='gray')
@@ -42,6 +63,11 @@ plt.title('Harris Image'), plt.xticks([]), plt.yticks([])
 #Plotting image copy Shi Tomasi
 plt.subplot(2, 2, 3), plt.imshow(cv2.cvtColor(imgShiTomasi, cv2.COLOR_BGR2RGB))
 plt.title('Shi Tomasi Image'), plt.xticks([]), plt.yticks([])
+
+#Plotting image ORB
+img2 = cv2.drawKeypoints(imgShiTomasi, kp, None, color=(0,255,0), flags=0)
+plt.subplot(2, 2, 4), plt.imshow(cv2.cvtColor(img2, cv2.COLOR_BGR2RGB))
+plt.title('ORB Image'), plt.xticks([]), plt.yticks([])
 
 plt.tight_layout()  # Automatically adjusts subplot spacing
 plt.show() 
