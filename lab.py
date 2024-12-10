b4173cc0e1407ec28a7d973cb4bba8a2ff3aee2b
trinv1 <trinityvillaruel@gmail.com>
2024-12-10 09:11:56 +0000

    split objects into specific classes

--- lab.py
+++ lab.py
@@ -1,9 +1,41 @@
-
 import cv2
 from ultralytics import YOLO
+from collections import defaultdict
+import numpy as np
 
 #Load pretained YOLOv8n model
 model = YOLO('yolov8n.pt')
 
-#Running inference on source
-results = model(source = 'vid1.mp4', show = True, conf = 0.4)
+# Open the video file 
+video_path = "vid1.mp4"
+cap = cv2.VideoCapture(video_path)
+
+# Loop through the video frames
+while cap.isOpened():
+    # Read a frame from the video
+    success, frame = cap.read()
+
+    if success:
+        # Run YOLO11 tracking on the frame, persisting tracks between frames
+        results = model.track(frame, persist=True)
+
+        # Visualize the results on the frame
+        annotated_frame = results[0].plot()
+
+        # Display the annotated frame
+        cv2.namedWindow('YOLOv11 Tracking', cv2.WINDOW_KEEPRATIO)
+        cv2.imshow('YOLOv11 Tracking', annotated_frame)
+        cv2.resizeWindow('YOLOv11 Tracking', 700, 500)
+
+        # Break the loop if 'q' is pressed
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+    else:
+        # Break the loop if the end of the video is reached
+        break
+
+# Release the video capture object and close the display window
+cap.release()
+cv2.destroyAllWindows()
+
+
